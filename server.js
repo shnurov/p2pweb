9de7c678e2bdc0e50e8e2cbad6e99783649c8b35
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-09-20 21:13:16 +0400

    feat: add actions

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const ACTIONS = require('./src/socket/actions');
 const PORT = process.env.PORT || 3001;
 
+function getClientRooms() {
+  const { rooms } = io.sockets.adapter;
+
+  return Array.from(rooms.keys());
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms(),
+  });
+}
+
 io.on('connection', (socket) => {
+  shareRoomsInfo();
   console.log('Socket connected');
 });
 
---
+++ src/socket/actions.js
@@ -0,0 +1,13 @@
+const ACTIONS = {
+  JOIN: 'join',
+  LEAVE: 'leave',
+  SHARE_ROOMS: 'share-rooms',
+  ADD_PEER: 'add-pear',
+  REMOVE_PEER: 'remove-pear',
+  RELAY_SDP: 'relay-sdp',
+  RELAY_ICE: 'relay-ice',
+  ICE_CANDIDATE: 'ice-candidate',
+  SESSION_DESCRIPTION: 'session-description',
+};
+
+module.exports = ACTIONS;
